6af4ec3a853493a139ba346bf82102140d0701c1
mnmari <mariananunoli@gmail.com>
2020-11-07 18:27:11 -0300

    Ex. 7

---
+++ Parte 1/ex7.js
@@ -0,0 +1,12 @@
+// Parte 1
+// Para todos os exercícios dessa primeira parte, considere que o seu input vão ser sempre variáveis no início do seu arquivo.
+//## Ex7
+//Faça um programa que receba uma frase com várias palavras separadas por espaço e retorne um array com cada uma delas.
+//Exemplo de frase: "Dóris sai de cima da pia!"
+
+"use strict"
+
+let Sentence = "Dóris sai de cima da pia!"
+
+let Array = Sentence.split(" ");
+console.log(Array);
