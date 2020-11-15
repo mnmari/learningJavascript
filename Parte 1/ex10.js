ad73cb638e3ea9a7494d6e4bdbdc051dd641c222
mnmari <mariananunoli@gmail.com>
2020-11-14 21:55:07 -0300

    Ex 10

---
+++ Parte 1/ex10.js
@@ -0,0 +1,20 @@
+// Parte 1
+// Para todos os exercícios dessa primeira parte, considere que o seu input vão ser sempre variáveis no início do seu arquivo.
+//## Ex10
+// Com o mesmo programa do exercício passado, agora retorne quais são as palavras que começam com a letra recebida.
+
+"use strict"
+
+let textData = "Dóris sai de cima da pia";
+let Character = "d";
+let Words = [];
+
+textData
+    .toLowerCase()
+    .split(" ")
+    .filter((item) =>{
+        if (item.charAt(0) === Character)
+            Words.push(item);
+    });
+
+console.log(Words);
