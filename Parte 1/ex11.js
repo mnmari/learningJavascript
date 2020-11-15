2c516bb9ffb8aacb89ae767b12a3fa2b68ab2d78
mnmari <mariananunoli@gmail.com>
2020-11-14 21:55:25 -0300

    Ex 11

---
+++ Parte 1/ex11.js
@@ -0,0 +1,19 @@
+// Parte 1
+// Para todos os exercícios dessa primeira parte, considere que o seu input vão ser sempre variáveis no início do seu arquivo.
+//## Ex11
+// Faça um programa que receba uma string e imprima ela ao contrário.
+
+"use strict"
+
+let textData = "Dóris sai de cima da pia";
+textData = textData.split("");
+
+let textBackwards = [];
+let textLength = textData.length;
+
+for(let i = 0; i < textLength; i++)
+    textBackwards.push(textData.pop()); 
+
+
+console.log(textBackwards.join(""));
+
