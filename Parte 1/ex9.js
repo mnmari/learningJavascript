6222268548088a970963b4aa0e8a5335c0b91ecb
mnmari <mariananunoli@gmail.com>
2020-11-14 23:07:05 -0300

    Ex 9

---
+++ Parte 1/ex9.js
@@ -0,0 +1,47 @@
+// Parte 1
+// Para todos os exercícios dessa primeira parte, considere que o seu input vão ser sempre variáveis no início do seu arquivo.
+//## Ex9
+// Faça um programa que receba um texto, uma letra e que conte quantas palavras do texto começam com a letra recebida.
+
+"use strict"
+
+//Forma 1
+let textData = "Dóris sai de cima da pia";
+let Character = "d";
+let Counter = 0;
+
+textData
+    .toLowerCase()
+    .split(" ")
+    .filter((item) =>{
+        if (item.charAt(0) === Character)
+            Counter++;
+    });
+
+console.log(Counter);
+
+//Forma 2
+/*let textData = "Dóris sai de cima da pia";
+let Character = "d";
+
+function prepareText(text){
+    text = text.toLowerCase();
+    
+    let textArray = [];
+    return textArray = text.split(" ");
+}  
+
+function countFirstLetters(text, character){
+    let occurrences = 0;
+
+    for (let i = 0; i < text.length; i++){
+        if (text[i].charAt(0) === character){
+            occurrences++;
+        }
+    }
+    return occurrences;
+}
+
+let preparedText = prepareText(textData);
+let qtyLetters = countFirstLetters(preparedText, Character);
+console.log(qtyLetters);*/
