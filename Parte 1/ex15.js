fcfab63a4a9c2fd5d3889043ef2b3e8fd6e89f42
mnmari <mariananunoli@gmail.com>
2020-11-14 21:56:14 -0300

    Ex 15

---
+++ Parte 1/ex15.js
@@ -0,0 +1,11 @@
+// Parte 1
+// Para todos os exercícios dessa primeira parte, considere que o seu input vão ser sempre variáveis no início do seu arquivo.
+// Ex15
+// Faça a mesma coisa do exercício anterior, mas com a raiz quadrada dos valores.
+
+"use strict"
+
+let Numbers = [4, 25, 36, 64, 81];
+let Numbers2 = Numbers.map(Math.sqrt);
+
+console.log(Numbers2);
