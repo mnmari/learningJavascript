78b2f520b14fa837cd2da5d2f8b5fa325cf7846e
mnmari <mariananunoli@gmail.com>
2021-01-04 21:06:08 -0300

    Ex3

--- Parte 2/data/birthday/birthday-repository.js
+++ Parte 2/data/birthday/birthday-repository.js
@@ -1,6 +1,7 @@
 'use strict';
 
 let file = require("../util/database-util");
+let Birthday = require("../../data/birthday/model/birthday");
 let fileName = "birthday.json";
 
 module.exports.write = async (data) => {
@@ -10,4 +11,10 @@ module.exports.write = async (data) => {
 	await file.writeDB(fileName, list);
 
 	return data;
+};
+
+module.exports.list = async () => {
+	let list = await file.readDB(fileName);
+
+	return list.map((item) => new Birthday(item.id, item.name, item.date, item.createdAt));
 };
